22430bc904217e3e22c6c770a0cead9b3bdaa05b
koenfelder <156391746+koenfelder@users.noreply.github.com>
2026-04-07 16:37:26 -0700

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, '.'),
     },
   },
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  }
 });
